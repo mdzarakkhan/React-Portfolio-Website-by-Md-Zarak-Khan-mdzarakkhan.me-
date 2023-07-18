9be0069ce72204759a99372aec777d917672ddfe
mdzarakkhan <mdzarakkhan@gmail.com>
2023-07-18 00:55:47 -0700

    changed data to map function

--- src/Pages/Projects/Projects.jsx
+++ src/Pages/Projects/Projects.jsx
@@ -1,14 +1,7 @@
 import React from 'react';
 import { motion } from "framer-motion";
-import overbrains from '../../assets/Images/websiteDesigns/overbrains.com.webp';
-import agrindtool from '../../assets/Images/websiteDesigns/agrindtool.webp';
-import davidleesbtq from '../../assets/Images/websiteDesigns/davidleesbtq.com.webp';
-import mdzarakkhan from '../../assets/Images/websiteDesigns/mdzarakkhan.webp';
-import mdzarakkhan2 from '../../assets/Images/websiteDesigns/mdzarakkhan2.webp';
-import giosyt from '../../assets/Images/websiteDesigns/giosyt.in.webp';
-import nurangi from '../../assets/Images/websiteDesigns/nurangi.com.webp';
-import dreamacake from '../../assets/Images/websiteDesigns/dreamacake.com.webp';
-import momnme from '../../assets/Images/websiteDesigns/momnme.pk.webp';
+import projectsData from '../../data/projectsData';
+
 
 
 const Projects = () => {
@@ -28,101 +21,18 @@ const Projects = () => {
 
                                 {/* <!-- Gallery --> */}
 
-
-
-                                <div className="row ">
-                                    <div className="col-lg-4 col-md-12  mb-4 mb-lg-0 ">
-                                        <a href="https://overbrains.com"> <img
-                                            src={overbrains}
-                                            className="w-100 shadow-1-strong rounded mb-4"
-                                            alt="agrintool"
-
-                                        />
-                                        </a>
-                                        <h6 className='text-center'>overbrains.com <span style={{ fontSize: '10px', color: "#4C9196" }}>(Business)</span></h6>
-                                    </div>
-
-                                    <div className="col-lg-4 col-md-12 mb-4 mb-lg-0 ">
-                                        <a href="https://agrindtool.com"><img
-                                            src={agrindtool}
-                                            className="w-100 shadow-1-strong rounded mb-4"
-                                            alt="agrintool"
-                                        />
-                                        </a>
-                                        <h6 className='text-center'>agrindtool.com <span style={{ fontSize: '10px', color: "#4C9196" }}>(eCommerce)</span></h6>
-                                    </div>
-
-                                    <div className="col-lg-4 mb-4 mb-lg-0">
-                                        <a href="https://davidleesbtq.com"><img
-                                            src={davidleesbtq}
-                                            className="w-100 shadow-1-strong rounded mb-4"
-                                            alt="davidleesbtq.com"
-                                        />
-                                        </a>
-                                        <h6 className='text-center'>davidleesbtq.com <span style={{ fontSize: '10px', color: "#4C9196" }}>(eCommerce)</span></h6>
-                                    </div>
-
-                                    <div className="col-lg-4 mt-4 mb-4 mb-lg-0">
-                                        <img
-                                            src={mdzarakkhan}
-                                            className="w-100 shadow-1-strong rounded mb-4"
-                                            alt="mdzarakkhan"
-                                        />
-                                        <h6 className='text-center'>mdzarakkhan <span style={{ fontSize: '10px', color: "#4C9196" }}>(Portfolio)</span></h6>
-                                    </div>
-
-                                    <div className="col-lg-4 mt-4 mb-4 mb-lg-0">
-                                        <img
-                                            src={mdzarakkhan2}
-                                            className="w-100 shadow-1-strong rounded mb-4"
-                                            alt="mdzarakkhan2"
-                                        />
-                                        <h6 className='text-center'>mdzarakkhan2 <span style={{ fontSize: '10px', color: "#4C9196" }}>(Portfolio)</span></h6>
-                                    </div>
-
-                                    <div className="col-lg-4 mt-4 mb-4 mb-lg-0">
-                                        <img
-                                            src={giosyt}
-                                            className="w-100 shadow-1-strong rounded mb-4"
-                                            alt="giosyt.in"
-                                        />
-                                        <h6 className='text-center'>giosyt.in <span style={{ fontSize: '10px', color: "#4C9196" }}>(eCommerce)</span></h6>
-                                    </div>
-
-                                    <div className="col-lg-4 mt-4 mb-4 mb-lg-0">
-                                        <a href="http://nurangi.com/"><img
-                                            src={nurangi}
-                                            className="w-100 shadow-1-strong rounded mb-4"
-                                            alt="giosyt.in"
-                                        />
-                                        </a>
-                                        <h6 className='text-center'>nurangi.com <span style={{ fontSize: '10px', color: "#4C9196" }}>(eCommerce Multi Vendor)</span></h6>
-                                    </div>
-
-                                    <div className="col-lg-4 mt-4 mb-4 mb-lg-0">
-                                        <a href="https://momnme.pk">
-                                            <img
-                                                src={momnme}
-                                                className="w-100 shadow-1-strong rounded mb-4"
-                                                alt="giosyt.in"
-                                            />
-                                        </a>
-                                        <h6 className='text-center'>momnme.pk <span style={{ fontSize: '10px', color: "#4C9196" }}>(eCommerce)</span></h6>
-                                    </div>
-
-                                    <div className="col-lg-4 mt-4 mb-4 mb-lg-0">
-                                        <a href="http://dreamacake.com/"><img
-                                            src={dreamacake}
-                                            className="w-100 shadow-1-strong rounded mb-4"
-                                            alt="giosyt.in"
-                                        />
-                                        </a>
-                                        <h6 className='text-center'>dreamacake.com <span style={{ fontSize: '10px', color: "#4C9196" }}>(eCommerce)</span></h6>
-                                    </div>
-
-
+                                <div className="row">
+                                    {projectsData.map((project) => (
+                                        <div className="col-lg-4 col-md-12 mb-5 mb-lg-0" key={project.id}>
+                                            <img src={project.src} className="w-100 shadow-1-strong rounded mb-4" alt={project.alt} />
+                                            <h6 className="text-center mb-4">
+                                                {project.text} <span style={{ fontSize: '10px', color: '#4C9196' }}>{project.subtext}</span>
+                                            </h6>
+                                        </div>
+                                    ))}
                                 </div>
-                                {/* <!-- Gallery --> */}
+
+                                {/* <!-- Gallery End--> */}
                             </div>
                         </div>
                     </div>
---
+++ src/data/projectsData.js
@@ -0,0 +1,76 @@
+import overbrains from '../assets/Images/websiteDesigns/overbrains.com.webp';
+import agrindtool from '../assets/Images/websiteDesigns/agrindtool.webp';
+import davidleesbtq from '../assets/Images/websiteDesigns/davidleesbtq.com.webp';
+import mdzarakkhan from '../assets/Images/websiteDesigns/mdzarakkhan.webp';
+import mdzarakkhan2 from '../assets/Images/websiteDesigns/mdzarakkhan2.webp';
+import giosyt from '../assets/Images/websiteDesigns/giosyt.in.webp';
+import nurangi from '../assets/Images/websiteDesigns/nurangi.com.webp';
+import momnme from '../assets/Images/websiteDesigns/momnme.pk.webp';
+import dreamacake from '../assets/Images/websiteDesigns/dreamacake.com.webp';
+
+const projectsData = [{
+    id: 1,
+    src: overbrains,
+    alt: 'overbrains',
+    text: 'overbrains.com',
+    subtext: '(Business)'
+},
+{
+    id: 2,
+    src: agrindtool,
+    alt: 'agrindtool',
+    text: 'agrindtool.com',
+    subtext: '(eCommerce)'
+
+},
+{
+    id: 3,
+    src: davidleesbtq,
+    alt: 'davidleesbtq',
+    text: 'davidleesbtq.com ',
+    subtext: '(eCommerce)'
+},
+{
+    id: 4,
+    src: mdzarakkhan,
+    alt: 'mdzarakkhan',
+    text: 'mdzarakkhan.com ',
+    subtext: '(Portfolio)'
+},
+{
+    id: 5,
+    src: mdzarakkhan2,
+    alt: 'mdzarakkhan2',
+    text: 'mdzarakkhan.com',
+    subtext: '(Portfolio)'
+},
+{
+    id: 6,
+    src: giosyt,
+    alt: 'giosyt.in',
+    text: 'giosyt.in',
+    subtext: '(eCommerce)'
+},
+{
+    id: 7,
+    src: nurangi,
+    alt: 'nurangi.com',
+    text: 'nurangi.com',
+    subtext: '(eCommerce Multi Vendor)'
+},
+{
+    id: 8,
+    src: momnme,
+    alt: 'momnme.pk',
+    text: 'momnme.pk',
+    subtext: '(eCommerce)'
+},
+{
+    id: 9,
+    src: dreamacake,
+    alt: 'dreamacake.com',
+    text: 'dreamacake.com',
+    subtext: '(eCommerce)'
+}];
+
+export default projectsData;
